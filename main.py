e2b441bebf1e3267b294c9a7594e51319ec60798
2172023 WILLYANDIE SAPUTRA <90822976+Willyandie@users.noreply.github.com>
2023-01-10 13:09:22 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    print("Login")
+    print("Registrasi")
+    validasi = str(input("Silakan input = "))
+    if (validasi == "login"):
+        email = str(input("Email = "))
+        password = str(input("Password = "))
+        pass
+    else:
+        # Menuju Halaman  Register
+        pass
+
+    return 0
+
+if __name__ == '__main__':
+    main()
