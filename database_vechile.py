a0554abc150f048f78be03bd7215e06b3d0b149c
Raafi Septyanto <90820954+RavioliRavioli52@users.noreply.github.com>
2023-01-23 15:09:02 +0700

    Update database_vechile.py

--- database_vechile.py
+++ database_vechile.py
@@ -1,3 +1,10 @@
+## Anggota Kelompok
+# 1. Willyandie Saputra - 2172023
+# 2. Tabitha E. Kotambunan - 2172021
+# 3. Cristianto Tri Arthurito - 2172027
+# 4. Raafi Septyanto - 2172041
+# 5. Stevanus Lucky Wijaya - 1872039
+
 import pickle
 from vechile import vechile
 
