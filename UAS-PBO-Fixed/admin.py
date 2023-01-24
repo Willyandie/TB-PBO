3ace42c07e1b82cfd3c04c49902e0c926b6cc83d
2172023 WILLYANDIE SAPUTRA <90822976+Willyandie@users.noreply.github.com>
2023-01-25 02:56:54 +0700

    Delete UAS-PBO-Fixed directory

--- UAS-PBO-Fixed/admin.py
+++
@@ -1,7 +0,0 @@
-class Admin:
-    def login_admin(self, email, password):
-        check = False
-        # Default email & password for admin
-        if(email == "admin" and password == "123"):
-            check = True
-        return check
